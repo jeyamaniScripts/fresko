252a9155e4e14dff06f32928ed5d450a93ebe17d
jeyamaniScripts <thejeyamani034@gmail.com>
2025-11-26 15:08:53 +0530

    final api redux update

--- src/components/product/Products.jsx
+++ src/components/product/Products.jsx
@@ -27,38 +27,45 @@ const Products = () => {
     dispatch(fetchMostRated());
     dispatch(fetchTrending());
     dispatch(fetchBestDiscounts());
-    dispatch(fetchByCategory("veg")); // example category (you can change later)
+    dispatch(fetchByCategory("veg"));
   }, [dispatch]);
 
   if (loading) return <p className="text-center pt-10">Loading...</p>;
   if (error) return <p className="text-center text-red-500 pt-10">{error}</p>;
 
   return (
-    <div className="pt-8 px-5 md:px-16 lg:px-24 xl:px-32 text-gray-700">
+    <div className="pt-4 px-5 md:px-16 lg:px-24 xl:px-32 text-gray-700">
       {/* Top Selling */}
-      <h1 className="text-xl md:text-2xl font-semibold mb-2 text-slate-800">
-        Top Selling Products
-      </h1>
-      <ProductList products={topSelling} />
+      <div className="mb-6">
+        <h1 className="sticky top-[70px] bg-white z-20 py-2 text-xl md:text-2xl font-semibold">
+          Top Selling Products
+        </h1>
+        <ProductList products={topSelling} />
+      </div>
 
       {/* Most Rated */}
-      <h1 className="text-xl md:text-2xl mt-6 font-semibold mb-2 text-slate-800">
-        Most Rated Products
-      </h1>
-      <ProductList products={mostRated} />
+      <div className="mb-6">
+        <h1 className="sticky top-[70px] bg-white z-20 py-2 text-xl md:text-2xl font-semibold">
+          Most Rated Products
+        </h1>
+        <ProductList products={mostRated} />
+      </div>
 
       {/* Trending */}
-      <h1 className="text-xl md:text-2xl mt-6 font-semibold mb-2 text-slate-800">
-        Trending Now
-      </h1>
-      <ProductList products={trending} />
+      <div className="mb-6">
+        <h1 className="sticky top-[70px] bg-white z-20 py-2 text-xl md:text-2xl font-semibold ">
+          Trending Now
+        </h1>
+        <ProductList products={trending} />
+      </div>
 
       {/* Best Discounts */}
-      <h1 className="text-xl md:text-2xl mt-6 font-semibold mb-2 text-slate-800">
-        Best Discounts
-      </h1>
-      <ProductList products={bestDiscounts} />
-      <ProductList products={categoryProducts} />
+      <div className="mb-6">
+        <h1 className="sticky top-[70px] bg-white z-20 py-2 text-xl md:text-2xl font-semibold ">
+          Best Discounts
+        </h1>
+        <ProductList products={bestDiscounts} />
+      </div>
     </div>
   );
 };
